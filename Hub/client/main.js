829bd9e40ba26ca63f5a136b74a45b1e63ed9840
Antonin Adert <antonin.adert@gmail.com>
2017-10-07 22:07:46 +0200

    Changed Hub hierarchy and points of entry

---
+++ Hub/client/main.js
@@ -0,0 +1,6 @@
+// Point of entry of our app
+import { Meteor } from 'meteor/meteor';
+import '/Hub/client/hubClient.js';
+Meteor.startup(() => {
+
+});
---
+++ Hub/server/main.js
@@ -0,0 +1,6 @@
+// Point of entry of our server
+import { Meteor } from 'meteor/meteor';
+import '/Hub/server/hubServer.js';
+Meteor.startup(() => {
+
+});
